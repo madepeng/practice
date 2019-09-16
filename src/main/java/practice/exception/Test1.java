e8fa76eab48498f79a256e37ab7ec3a7ffaaa608
madepeng_bxbank <madepeng@aibank.com>
2019-09-16 19:34:17 +0800

    change

---
+++ src/main/java/practice/exception/Test1.java
@@ -0,0 +1,31 @@
+package practice.exception;
+
+import org.springframework.cglib.core.CollectionUtils;
+
+/**
+ * @author madepeng
+ * @description: TODO
+ * @date 2019/9/12 - 17:43
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        g();
+    }
+
+    public static void g(){
+        System.out.println("t start");
+        try {
+            t();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        System.out.println("t end");
+    }
+
+    public static void t(){
+        System.out.println(2/2);
+        System.out.println(1/0);
+        System.out.println(2/3);
+        System.out.println();
+    }
+}
--- src/main/java/practice/multiThread/exceptionTest/ExecuteCaught.java
+++ src/main/java/practice/multiThread/exceptionTest/ExecuteCaught.java
@@ -12,9 +12,12 @@ public class ExecuteCaught
 {
     public static void main(String[] args)
     {
+        Thread.currentThread().setUncaughtExceptionHandler((t, e) -> System.out.println("自定义异常处理器：" + t.getName() + e.toString()));
         ExecutorService exec = Executors.newCachedThreadPool();
         exec.execute(new ThreadPoolTask());
+        int i = 1/0;
         exec.shutdown();
+        throw new RuntimeException("madep");
     }
 }
 
@@ -23,9 +26,10 @@ class ThreadPoolTask implements Runnable
     @Override
     public void run()
     {
-        Thread.currentThread().setUncaughtExceptionHandler((t, e) -> System.out.println("自定义异常处理器：" + t.getName() + e.toString()));
+//        Thread.currentThread().setUncaughtExceptionHandler((t, e) -> System.out.println("自定义异常处理器：" + t.getName() + e.toString()));
         System.out.println(3/2);
-        System.out.println(3/0);
+//        System.out.println(3/0);
         System.out.println(3/1);
+//        throw new RuntimeException("fdsfdsf");
     }
 }
