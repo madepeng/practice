9fbf920890f4ee778dd5a9a9b501e2ecb90ec8c0
madepeng <madepeng@aibank.com>
2020-01-17 17:48:03 +0800

    验证类属性构造顺序

---
+++ src/main/java/practice/initorder/Test.java
@@ -0,0 +1,14 @@
+package practice.initorder;
+
+/**
+ * @ClassName Test
+ * @Description: TODO
+ * @Author madepeng
+ * @Date 2020/1/17
+ * @Version V1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
