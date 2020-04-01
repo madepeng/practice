4a8050a35a20e1a8b7539f9e722aa6ebf04c8c9a
madepeng <madepeng@aibank.com>
2020-04-01 19:00:07 +0800

    验证类属性构造顺序

---
+++ src/main/java/practice/stream/Reduce.java
@@ -0,0 +1,14 @@
+package practice.stream;
+
+/**
+ * @ClassName Reduce
+ * @Description: TODO
+ * @Author madepeng
+ * @Date 2020/4/1
+ * @Version V1.0
+ **/
+public class Reduce {
+    public static void main(String[] args) {
+
+    }
+}
