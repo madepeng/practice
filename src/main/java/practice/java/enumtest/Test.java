d74945b99fcafce0e4650f355d09ebdb00585dfc
madepeng <madepeng@aibank.com>
2020-05-18 15:27:05 +0800

    enum

---
+++ src/main/java/practice/java/enumtest/Test.java
@@ -0,0 +1,14 @@
+package practice.java.enumtest;
+
+/**
+ * @ClassName Test
+ * @Description: TODO
+ * @Author madepeng
+ * @Date 2020/5/18
+ * @Version V1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(OrderTypeEnum.Consume.name());
+    }
+}
