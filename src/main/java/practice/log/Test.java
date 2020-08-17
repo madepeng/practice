6f45cd1aa112a4e58c2fe38c26b00f1b8bff6612
madepeng <madepeng@aibank.com>
2020-08-17 14:25:21 +0800

    logback配置

--- src/main/java/practice/log/Test.java
+++ src/main/java/practice/log/Test.java
@@ -1,7 +1,9 @@
 package practice.log;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * @ClassName Test
@@ -13,9 +15,18 @@ import org.slf4j.LoggerFactory;
 public class Test {
     private final static Logger logger = LoggerFactory.getLogger(Test.class);
 
+
     public static void main(String[] args) {
+        MDC.put("transId", "67539674385");
         logger.info("logback 成功了");
         logger.error("logback 成功了");
         logger.debug("logback 成功了");
+
+        Integer[] arr = {1,2,3};
+        System.out.println(ArrayUtils.toString(arr));
+
+
+        Thread thread = new Thread(() -> logger.info("thread1 running"), "thread1name");
+        thread.start();
     }
 }
